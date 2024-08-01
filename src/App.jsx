0360ff0dd3d0891f5f2c94de24c198c70ddafe81
Eshpolatov-Sardor <167778318+Eshpolatov-Sardor@users.noreply.github.com>
2024-08-02 00:17:46 +0500

    git pull

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
         <Route path="/product-cart" element={<ProductCart cart={cart} length={cart.length} />} />
 
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </React.Fragment>
   );
 };
